faca6f21116791051b7519d0128947e28540f548
Tatiana-ast <60969151+Tatiana-ast@users.noreply.github.com>
2021-10-08 13:25:52 +0200

    Add files via upload

---
+++ PruebaWeb/PruebaWeb/Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaWeb.Models
+{
+    public class Producto
+    {
+        public int cantidad { get; set; }
+        public string nombre { get; set; }
+        public int precio { get; set; }
+        public string tipo { get; set; }
+    }
+}
